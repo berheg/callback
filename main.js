80c55d63345b33878ff161fffb6fe0547cb26863
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-07-12 21:43:19 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const posts = [
+    {title: 'Post one', body: 'This is post one'},
+    {title: 'Post two', body: 'This is post two'}
+];
+function getPosts(){
+    setTimeout(() =>{
+        let outPut = '';
+        posts.forEach((post, index) => {
+            outPut += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = outPut;
+    },1000);
+}
+function creatPost(post){
+    setTimeout(() => {
+        posts.push(post);
+    },2000);
+}
+getPosts();
+creatPost({ title: 'Post Three', body: 'This is post three'});
